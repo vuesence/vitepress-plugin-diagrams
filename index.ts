6c3b06d298ae2ddfe28044e8c494a07f07f95486
Ruslan Makarov <ruslan.makarov@gmail.com>
2025-02-13 15:25:13 +0300

    Remove not-needed dependency and update package configuration

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import * as fs from "node:fs";
 import * as path from "node:path";
-import fetch from "node-fetch";
 import { SUPPORTED_DIAGRAM_TYPES, type DiagramType } from "./constants";
 
 import {
