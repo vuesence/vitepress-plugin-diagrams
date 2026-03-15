b4a1d397c49b3399d11ded3484c37dd041c40db9
towerhe <towerhe@users.noreply.github.com>
2026-03-15 05:27:20 +0000

    deploy: fe74856b004fdc0254e25a1443a0f5a69ba626bf

--- demo/assets/file-imports.md.CRdtejMl.lean.js
+++ demo/assets/file-imports.md.CRdtejMl.lean.js
@@ -9,7 +9,7 @@ import{_ as r,c as l,o as n,ae as i,j as a,a as s}from"./chunks/framework.CkhtPn
         if (!isFullscreen) {
           figure.classList.add('vpd-diagram--fullscreen');
         }
-      `},[a("img",{src:"/vitepress-plugin-diagrams/demo/diagrams/bpmn-file-bpmn-1-c327ea771c28171d32a5c9278bba92de.svg",alt:"bpmn Diagram",class:"vpd-diagram-image"})],-1),a("h3",{id:"mermaid-from-file",tabindex:"-1"},[s("Mermaid from File "),a("a",{class:"header-anchor",href:"#mermaid-from-file","aria-label":'Permalink to "Mermaid from File"'},"​")],-1),a("figure",{class:"vpd-diagram vpd-diagram--mermaid",onclick:`
+      `},[a("img",{src:"/vitepress-plugin-diagrams/demo/diagrams/bpmn-file-bpmn-1-ecd7a5f225a1a340fd02ce5c7fe8fcc4.svg",alt:"bpmn Diagram",class:"vpd-diagram-image"})],-1),a("h3",{id:"mermaid-from-file",tabindex:"-1"},[s("Mermaid from File "),a("a",{class:"header-anchor",href:"#mermaid-from-file","aria-label":'Permalink to "Mermaid from File"'},"​")],-1),a("figure",{class:"vpd-diagram vpd-diagram--mermaid",onclick:`
         const figure = this;
         const isFullscreen = figure.classList.contains('vpd-diagram--fullscreen');
 
@@ -20,7 +20,7 @@ import{_ as r,c as l,o as n,ae as i,j as a,a as s}from"./chunks/framework.CkhtPn
         if (!isFullscreen) {
           figure.classList.add('vpd-diagram--fullscreen');
         }
-      `},[a("img",{src:"/vitepress-plugin-diagrams/demo/diagrams/mermaid-file-mermaid-1-1cfe2b892f91b4a133700765e2059aff.svg",alt:"mermaid Diagram",class:"vpd-diagram-image"})],-1),a("h3",{id:"plantuml-from-file",tabindex:"-1"},[s("PlantUML from File "),a("a",{class:"header-anchor",href:"#plantuml-from-file","aria-label":'Permalink to "PlantUML from File"'},"​")],-1),a("figure",{class:"vpd-diagram vpd-diagram--plantuml",onclick:`
+      `},[a("img",{src:"/vitepress-plugin-diagrams/demo/diagrams/mermaid-file-mermaid-1-cb4ed308bbe5a7494f10a080182f6179.svg",alt:"mermaid Diagram",class:"vpd-diagram-image"})],-1),a("h3",{id:"plantuml-from-file",tabindex:"-1"},[s("PlantUML from File "),a("a",{class:"header-anchor",href:"#plantuml-from-file","aria-label":'Permalink to "PlantUML from File"'},"​")],-1),a("figure",{class:"vpd-diagram vpd-diagram--plantuml",onclick:`
         const figure = this;
         const isFullscreen = figure.classList.contains('vpd-diagram--fullscreen');
 
@@ -31,4 +31,4 @@ import{_ as r,c as l,o as n,ae as i,j as a,a as s}from"./chunks/framework.CkhtPn
         if (!isFullscreen) {
           figure.classList.add('vpd-diagram--fullscreen');
         }
-      `},[a("img",{src:"/vitepress-plugin-diagrams/demo/diagrams/plantuml-file-plantuml-1-a1ef0a665a113d8c8dd4ab665a0aca94.svg",alt:"plantuml Diagram",class:"vpd-diagram-image"})],-1),i("",18)]))}const f=r(t,[["render",o]]);export{u as __pageData,f as default};
+      `},[a("img",{src:"/vitepress-plugin-diagrams/demo/diagrams/plantuml-file-plantuml-1-4fb509e8da23fe17cad2b141015340ba.svg",alt:"plantuml Diagram",class:"vpd-diagram-image"})],-1),i("",18)]))}const f=r(t,[["render",o]]);export{u as __pageData,f as default};
